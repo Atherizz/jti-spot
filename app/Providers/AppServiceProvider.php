f339f06319150faa64a99caf1256d888f7303614
Atherizz <saveroathallah86@gmail.com>
2026-03-06 10:21:04 +0700

    chore: backup missing files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Http\Services\SiakadService;
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(SiakadService::class, function ($app) {
+            $siakadUrl = env('SIAKAD_URL', 'https://siakad.polinema.ac.id');
+            return new SiakadService($siakadUrl);
+        });
     }
 
     /**
@@ -19,6 +25,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('admin', function (User $user) {
+            return $user->role == 'admin';
+        });
+
+        Gate::define('student', function (User $user) {
+            return $user->role == 'student' || $user->role == 'class_rep';
+        });
+
+        Gate::define('class_rep', function (User $user) {
+            return $user->role == 'class_rep';
+        });
+
+
     }
 }
